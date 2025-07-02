e641ebd11eb7afba6fc796b8a3e40ae69b2ad9c3
Ashutosh Singh <1709ashutosh@gmail.com>
2025-07-02 12:03:51 +0530

    update

--- backEnd/src/routes/user.js
+++ backEnd/src/routes/user.js
@@ -4,6 +4,8 @@ const ConnectionRequestModel = require("../models/connectionRequest");
 const userRouter = express.Router();
 
 
+const USER_SAFE_DATA="firstName lastName photoUrl age gender about skills";
+
 // get All the pending connectionRequest for the logged in user
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     try {
@@ -28,4 +30,30 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     }
 })
 
+
+userRouter.get("/user/connection",
+    userAuth,async(req,res)=>{
+        // elon =>akshay=>Accepted
+        // mark=>elon=>accepted
+
+        try{
+
+            const loggedInUser=req.user;
+            const connectionReqest= await ConnectionRequestModel.find({
+                $or:[
+                    {toUserId:loggedInUser._id,status:"accepted"},
+                    {fromUserId:loggedInUser._id,status:"accepted"}
+                ]
+            }).populate("fromUserId",USER_SAFE_DATA)
+            
+            const data=connectionReqest.map(row=>row.fromUserId);
+            res.json({message:"Coneccction found Sucessfull" ,
+                Data:data
+            })
+
+        }catch(error){
+            res.status(400).send("Error - " + error.message)
+        }
+    }
+)
 module.exports = userRouter;
