f47a7e36fc6c7430e7c9b9004c1c8c4437ed4cf8
Ashutosh Singh <1709ashutosh@gmail.com>
2025-07-04 20:55:12 +0530

    front End started

---
+++ devtinder-web/README.md
@@ -0,0 +1,7 @@
+# DevTinder 
+
+-Created a Vite + React application
+-Remove uneccessary code a HELLO WORLD in APP.jsx
+-Install Tailwind CSS
+-Install Daisy UI
+-Add NavBar component in App.jsx
---
+++ devtinder-web/src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from './NabBar'
+
+function App() {
+
+  return (
+    <>
+   <NavBar/>
+      <h1 className="text-red-800 text-3xl underline">Hello world</h1>
+    </>
+  )
+}
+
+export default App
+
