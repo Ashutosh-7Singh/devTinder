e779e19f534769aadef85fd5365aef3ec0852586
Ashutosh Singh <1709ashutosh@gmail.com>
2025-07-14 02:13:21 +0530

    update

--- backEnd/src/config/database.js
+++ backEnd/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");;
 
 const connectbDb=async()=>{
     // await mongoose.connect("mongodb://localhost:27017/devBaba")
-    console.log(process.env.DB_CONNECTION_SECRET);
+    // console.log(process.env.DB_CONNECTION_SECRET);
     
     await mongoose.connect(process.env.DB_CONNECTION_SECRET)
 }
